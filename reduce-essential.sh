4feb7c411dfe5cd169f7f285bc61f9c3ac973291
h.thien <h.thien@callassoftware.com>
2024-12-03 17:21:52 +0100

    added reduce-essential.sh

--- reduce-essential.sh
+++ reduce-essential.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# inspired by https://help.callassoftware.com/m/pdftoolbox/l/793898-file-components-and-their-use-in-pdftoolbox-sdk
 rm -r -f doc var
 mkdir -p var/PDFAExtSchema
 pushd etc
